8bff8983c55826b65a74096068c882afd186ef9d
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-11-15 11:48:32 -0600

    se hicieron cambios en la versiÃon responsive del index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,14 @@
 <header class="bg-blue">
     <div class="container pad">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 pad-1">
+            <div class="col-lg-6 col-md-12 col-sm-12 pad-1 pad-2">
                 <h1 style="color: #fff;">Prepárate con Sapius <br>
                     Cursos online para el <span class=color-lowblue>EGEL</span> 
                 </h1>
                 <p style="color: #fff;">"Tu formación, es nuestra pasión"</p>
                 <a href="" class="btn btn-primary">Comenzar</a>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="col-lg-6 col-md-12 col-sm-12 d-none d-sm-none d-md-none d-lg-block">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                       <div class="carousel-item active">
@@ -30,11 +30,164 @@
         </div>
     </div>
 </header>
+<section class="pt-3 d-none d-sm-none d-md-none d-lg-block">
+  <div class="pt-5">
+    <div class="container">
+      <h1 class="color-gray text-center pb-5" style="line-height: 37px; font-weight:600;">
+        ¿Cuál es tu objetivo? <br> <span style="font-size:18px; font-weight:300; color: gray;">Selecciona el objetivo que quieras conseguir</span>
+      </h1>
+      <ul class="nav nav-tabs nav-justified" id="pills-tab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active text-title2" id="pills-prepa-tab" data-toggle="pill" href="#prepa">
+                <span>Quiero entrar <br> a la prepa</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-title2" id="pills-universidad-tab" data-toggle="pill" href="#universidad">
+                <span>Quiero entrar <br> a la Universidad</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-title2" id="pills-egel-tab" data-toggle="pill" href="#egel">
+                <span>Quiero aprobar <br> el EGEL Plus</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-title2" id="pills-maestria-tab" data-toggle="pill" href="#maestria">
+                <span>Quiero aprobar <br> la Maestría</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-title2" id="pills-enarm-tab" data-toggle="pill" href="#enarm">
+                <span>Quiero aprobar <br> el ENARM</span>
+            </a>
+        </li>
+    
+    </ul>
+    </div>
+
+    <div class="tab-content" id="pills-tabContent">
+        <div class="tab-pane fade show active" id="prepa" role="tabpanel">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 offset-md-1 pad-1">
+                        <div class="contaner-title-tabs">
+                            <h1 class="juntos">Juntos lograremos tu<br> admisión a la <span class="color-text">Preparatoria</span></h1>
+                            <p class="text-sub-title1"> En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+                        </div>
+                        <div class="container-button">
+                            <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="{{ asset('img/v1/1.png') }}" class="img-fluid" alt="Foto 1"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade show" id="universidad" role="tabpanel">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-6 offset-md-1 pad-1">
+                    <div class="contaner-title-tabs">
+                        <h1 class="juntos">Juntos lograremos tu<br> admisión a la <span class="color-text">Universidad</span></h1>
+                        <p class="text-sub-title1">En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+                    </div>
+                    <div class="container-button">
+                        <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('img/v1/2.png') }}" class="img-fluid" alt="Foto 1"/>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="tab-pane fade show" id="egel" role="tabpanel">
+          <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 offset-md-1 pad-1">
+                    <div class="contaner-title-tabs">
+                        <h1 class="juntos">Juntos lograremos <br> aprobar el <span class="color-text">EGEL Plus</span></h1>
+                        <p class="text-sub-title1"> En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+                    </div>
+                    <div class="container-button">
+                        <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('img/v1/3.png') }}" class="img-fluid" alt="Foto 1"/>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="tab-pane fade show" id="maestria" role="tabpanel">
+          <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 offset-md-1 pad-1">
+                    <div class="contaner-title-tabs">
+                        <h1 class="juntos">Juntos lograremos <br> aprobar la <span class="color-text">Maestría</span></h1>
+                        <p class="text-sub-title1"> En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+                    </div>
+                    <div class="container-button">
+                        <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('img/v1/4.png') }}" class="img-fluid" alt="Foto 1"/>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="tab-pane fade show" id="enarm" role="tabpanel">
+          <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 offset-md-1 pad-1">
+                    <div class="contaner-title-tabs">
+                        <h1 class="juntos">Juntos lograremos <br> aprobar el <span class="color-text">ENARM</span></h1>
+                        <p class="text-sub-title1"> En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+                    </div>
+                    <div class="container-button">
+                        <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('img/v1/5.png') }}" class="img-fluid" alt="Foto 1"/>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</section >
+<!--Movil objetivos-->
+<section class="d-block d-sm-block d-md-block d-lg-none pt-5">
+  <div class="container">
+    <h1 class="color-gray text-center pb-3" style="line-height: 37px; font-weight:600;">
+      ¿Cuál es tu objetivo? <br> <span style="font-size:18px; font-weight:300; color: gray;">Selecciona el objetivo que quieras conseguir</span>
+    </h1>
+  </div>
+  <div class="bg-blue pt-4 pb-4">
+    <div class="container">
+      <div class="col-12">
+        <h2 class="juntos text-center">Juntos lograremos tu<br> admisión a la <span class="color-text">Preparatoria</span></h2>
+        <p class="text-sub-title1 text-center"> En esta curso obtendrás todo lo que necesitas para aprobar el examen.</p>
+      </div>
+      <div class="col-12 text-center">
+        <img src="{{ asset('img/v1/1-m.png') }}" class="img-fluid" alt="Foto 1"/>
+      </div>
+      <div class="col-12 text-center">
+        <a href="{{ route('register') }}" class="button-title-tabs btn btn-primary">COMENZAR</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!--end Movil objetivos-->
 <section id="orgullos-sapius">
     <div class="bg-naranja pt-5">
       <div class="container">
         <div class="row orgullo-sapius">
-          <div class="col-md-6 col-lg-6 col-sm-12">
+          <div class="col-md-12 col-lg-6 centro-card col-sm-12">
             <div class="orgullo-txt">
               <h1 class="color-gray">
                   <strong>Conoce nuestros <br>
@@ -44,7 +197,7 @@
                 <p class="color-gray">Selecciona el objetivo que quieras conseguir</p>
             </div>
           </div>
-          <div class="col-md-6 col-lg-6 col-sm-12 text-center">
+          <div class="col-md-12 col-lg-6 col-sm-12 centro-card text-center">
             <div class="orgullo-card">
               <div class="card">
                 <div class="cabecera-orgullo">
@@ -69,32 +222,32 @@
       <h2 class="color-gray text-center pt-5 pb-4">La plataforma diferente para ser excelente</h2>
       <div class="row">
         <div class="col-md-4 col-lg-4 col-sm-12">
-          <div class="row">
-            <div class="col-2">
-              <img src="{{asset('img/v1/icon/1.svg')}}" width="37" alt="">
+          <div class="row centro-card">
+            <div class="col-md-12 col-lg-2 col-sm-12">
+              <i class="fas fa-check-circle" style="font-size: 40px; color:#ED6A5A;"></i>
             </div>
-            <div class="col-10">
+            <div class="col-md-12 col-lg-10 col-sm-12">
                 <h5 class="color-gray">El 99.1% de nuestros estudiantes acreditan</h5>
             </div>
           </div>
         </div>
         <div class="col-md-4 col-lg-4 col-sm-12">
-          <div class="row">
-            <div class="col-2">
-              <img src="{{asset('img/v1/icon/1.svg')}}" width="37" alt="">
+          <div class="row centro-card">
+            <div class="col-md-12 col-lg-2 col-sm-12">
+              <i class="fas fa-user-graduate" style="font-size: 40px; color:#ED6A5A;"></i>
             </div>
-            <div class="col-10">
+            <div class="col-md-12 col-lg-10 col-sm-12">
               <h5 class="color-gray">Nuestro método SUMA
                 maximiza tus resultados</h5>
             </div>
           </div>
         </div>
         <div class="col-md-4 col-lg-4 col-sm-12">
-          <div class="row">
-            <div class="col-2">
-              <img src="{{asset('img/v1/icon/1.svg')}}" width="37" alt="">
+          <div class="row centro-card">
+            <div class="col-md-12 col-lg-2 col-sm-12">
+              <i class="fas fa-book-reader" style="font-size: 40px; color:#ED6A5A;"></i>
             </div>
-            <div class="col-10">
+            <div class="col-md-12 col-lg-10 col-sm-12">
               <h5 class="color-gray">El curso más <br class="d-none d-sm-none d-md-block">
                 completo del mercado</h5>
             </div>
@@ -105,7 +258,7 @@
   </section>
   <section id="maestros">
     <div class="bg-blue mt-5">
-      <div class="container"> <!--Slider-->
+      <div class="container-fluid"> <!--Slider-->
         <h2 class="text-center pt-5 pb-4" style="color: #fff;">Conoce a nuestros docentes</h2>
         <div class="row text-center maestros pb-5">
           <div class="col-md-3 col-lg-3 col-sm-6">
@@ -142,17 +295,18 @@
           </div>
         </div>
       </div> <!--END Slider-->
+      
     </div>
   </section>
   <section id="modalidades">
     <div class="container pt-5 pb-5">
       <div class="row text-center">
-        <div class="col-md-6 col-sm-12 p-info">
+        <div class="col-lg-6 col-md-12 col-sm-12 p-info">
           <h2>¿Quieres más información?</h5>
-          <p class="color-gray ">Nuestros asesores están felices de ayudarte <br><br></p>
+          <p class="color-gray ">Nuestros asesores están felices de ayudarte <br class="d-none d-sm-none d-md-none d-lg-block"><br class="d-none d-sm-none d-md-none d-lg-block"></p>
           <a href="" class="btn btn-primary">Enviar mensaje</a>
         </div>
-        <div class="col-md-6 col-sm-12 p-info">
+        <div class="col-lg-6 col-md-12 col-sm-12 p-info">
           <h2>Modalidad Presencial</h5>
           <p class="color-gray">Nuestro cuerpo docente esta disponible para consultas, asesorías, clases, pláticas, conferencias y ponencias.</p>
           <a href="" class="btn btn-primary">Más información</a>
