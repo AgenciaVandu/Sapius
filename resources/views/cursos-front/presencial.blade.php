071c7cc90db47cb288c4d0bf4ccee6feac15ab41
Alvar <alvarbu@gmail.com>
2022-08-04 18:58:11 -0500

    se aplican cambios en vista presencial

--- resources/views/cursos-front/presencial.blade.php
+++ resources/views/cursos-front/presencial.blade.php
@@ -14,7 +14,29 @@
 </header>
 <section id="incluido" class="bg-naranja-3">
     <div class="container">
-        <div class="row pt-5 align-items-center">
+        <div class="row">
+            <div class="col-lg-5 col-md-12 col-sm-12 text-center m-auto">
+                <img src="{{asset('img/presencial.png')}}" class="img-fluid" alt="Clases presenciales EGEL PLUS">
+            </div>
+            <div class="col-lg-7 col-md-12 col-sm-12 m-auto incluido__info">
+                <h5>Tasa de aprobados del 99.1%</h5>
+                <p>Contamos con la tasa de aprobados más alta a nivel nacional, con una acreditación del 99.1%, con resultados de excelencia.</p>
+                <hr>
+                <h5>Más de 1200 estudiantes nos avalan</h5>
+                <p>En la modalidad presencial contamos con más de 1200 estudiantes, satisfechos con sus resultados.</p>
+                <hr>
+                <h5>Profesores expertos en el área con programas actualizados</h5>
+                <p>Nuestros profesores cuentan con la máxima experiencia en docencia, cuentan con un amplio conocimiento teórico – practico de la especialidad.</p>
+                <hr>
+                <h5>Contamos con descuentos por grupos y pago anticipado</h5>
+                <p>Programa de descuentos por grupos de estudiantes (Descuento dependiendo el número) y descuento por pago anticipado.</p>
+                <hr>
+                <h5>Contamos con descuentos por grupos y pago anticipado</h5>
+                <p>Nuestros programas educativos cuentan con un grupo limitado de estudiantes con la finalidad de brindar atención personalizada a cada estudiante, para la obtención de excelentes resultados.</p>
+                <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#sapius-info">¡Solicita una clase muestra!</button>
+            </div>
+        </div>
+      <!--   <div class="row pt-5 align-items-center">
             <div class="col-lg-6 col-md-12 col-sm-12 pt-5">
                 <h2>Tasa de aprobados <br> del 99.1%</h2>
                 <p>Contamos con la tasa de aprobados más alta a nivel nacional,<br> con una acreditación del 99.1%, con resultados de excelencia.</p>
@@ -53,7 +75,7 @@
             <div class="col-lg-6 col-md-12 col-sm-12 pt-5">
                 <img src="{{asset('img/v1/estudiantes/5.jpg')}}" class="img-fluid" alt="">
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
 <section id="experiencia">
@@ -61,14 +83,37 @@
         <div class="container text-center">
             <div class="posicion">
                 <h1 style="color: #fff;">Más de 10 años de excelencia nos respaldan</h1>
-                <a href="" class="btn btn-primary">COMENZAR</a>
+                <a href="{{ route('register') }}" class="btn btn-primary">COMENZAR</a>
             </div>
         </div>
     </div>
 </section>
 <section id="prueba" >
     <div class="container justify-content-center">
-        <div class="row pt-5">
+    <div class="row pt-5">
+        <div class="col-md-6 col-lg-6 col-sm-12">
+          <div class="centro-card text-center">
+            <div class="col-12 pb-3">
+            <img src="{{asset ('img/v1/icon/exni/calendario.png')}}" class="img-fluid" alt="">
+            </div>
+            <div class="col-12">
+                <h5 class="color-gray" style="font-weight: 400">Contamos con diferentes <br>horarios disponibles</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-6 col-sm-12">
+          <div class="centro-card text-center">
+            <div class="col-12 pb-3">
+            <img src="{{asset ('img//v1/icon/exni/alumno.png')}}" class="img-fluid" alt="">
+            </div>
+            <div class="col-12">
+              <h5 class="color-gray" style="font-weight: 400">Grupos <br>
+                reducidos</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+       <!--  <div class="row pt-5">
             <div class="col-md-6 col-lg-6 col-sm-12">
                 <div class="row">
                     <div class="col-4" style="text-align: right">
@@ -89,7 +134,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
 <section id="orgullos-sapius">
