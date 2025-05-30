1c031e4dc2a9dbf1df58c8d0452dd12cd3b1ac10
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-05-30 11:17:26 -0600

    Cambios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -429,13 +429,13 @@
     @push('js')
         <script>
             new Glide('.glide', {
-                perView: 2,
+                perView: 3,
                 gap: 10,
                 autoplay: 2000,
                 bound: true,
                 breakpoints: {
                     1200: {
-                        perView: 2,
+                        perView: 3,
                         gap: 10
                     },
                     992: {
