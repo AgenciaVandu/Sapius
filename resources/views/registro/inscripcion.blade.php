fd24e33838fc34899c063a72366b631be2d96382
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-07-17 18:41:44 -0600

    Aprovacion de certificados openpay pago con tarjeta

--- resources/views/registro/inscripcion.blade.php
+++ resources/views/registro/inscripcion.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.adminmart.default')
 
+@section('breadcrumb')
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-12 align-self-center">
+                <h2 class="page-title text-truncate text-dark font-weight-medium mb-1">
+                    Pasarela de pagos</h2>
+                {{-- <h3 class="page-title text-truncate text-dark font-weight-medium mb-1"></h3> --}}
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('content')
     @php
         $precio = $curso->precio;
