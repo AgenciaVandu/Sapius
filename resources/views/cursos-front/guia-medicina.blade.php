3734d66138daa690161720a0fbd20374fbd16f16
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-07-30 23:06:51 -0600

    Cambios

--- resources/views/cursos-front/guia-medicina.blade.php
+++ resources/views/cursos-front/guia-medicina.blade.php
@@ -7,7 +7,7 @@
         content="{{ $meta_keywords ?? 'Cursos Egel, Cursos Enarm, Cursos Exani I, Cursos Exani II, Cursos Exani III, cursos para aprobar el EGEL, cómo aprobar el Exani' }}">
     <meta name="robots" content="{{ $meta_robots ?? 'index, follow' }}">
     <link rel="canonical" href="{{ $meta_canonical ?? url()->current() }}">
-    <meta name="image" content="{{ $meta_image ?? asset('img/v1/logo-sapius.svg') }}">
+    <meta name="image" content="{{ $meta_image ?? asset('vendor/adminmart/assets/images/big/cursos.png') }}">
 @endpush
 @push('css')
     <link rel="stylesheet" href="{{ asset('css/exani3.css') }}">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta name="description" content="{{ $meta_description ?? 'Prepárate con Sapius Cursos online' }}">
     <meta name="keywords" content="{{ $meta_keywords ?? 'Cursos Egel, Cursos Enarm, Cursos Exani I, Cursos Exani II, Cursos Exani III, cursos para aprobar el EGEL, cómo aprobar el Exani' }}">
     <meta name="robots" content="{{ $meta_robots ?? 'index, follow' }}">
-    <meta name="image" content="{{ $meta_image ?? asset('img/v1/logo-sapius.svg') }}">
+    <meta name="image" content="{{ $meta_image ?? asset('vendor/adminmart/assets/images/big/cursos.png') }}">
 
     <link rel="canonical" href="{{ $meta_canonical ?? url()->current() }}">
 @endpush
