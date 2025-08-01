3aa557c3752b0d6a5172994431f7b3deaba63e6a
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-07-31 23:11:48 -0600

    Tag Google

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -2,6 +2,18 @@
 <html lang="es">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0PBBHC1SY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-Q0PBBHC1SY');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
