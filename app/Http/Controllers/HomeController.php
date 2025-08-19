636b93643d863c35a110efbb5f908408754049a6
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-19 10:10:37 -0600

    correccion en la visualizacion del admin para la aprobacion de los alumnos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -88,7 +88,7 @@ class HomeController extends Controller
 
     public function admin()
     {
-        $cursos = CursoProgramado::with('Curso')->where('fecha_inicio', '<=', date('Y-m-d H:i:s'))
+        $cursos = CursoProgramado::with('Curso')/* ->where('fecha_inicio_venta', '<=', date('Y-m-d H:i:s')) */
             ->where('fecha_fin', '>=', date('Y-m-d H:i:s'))->get();
         return view('admin.home')->with('cursos', $cursos);
     }
