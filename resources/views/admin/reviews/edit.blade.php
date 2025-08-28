925defa142a160adc5e1bdcf0b0b2b229e0feec8
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-28 09:45:19 -0600

    Ajuste de las reviews

--- resources/views/admin/reviews/edit.blade.php
+++ resources/views/admin/reviews/edit.blade.php
@@ -47,16 +47,24 @@
                         </div> --}}
                         <div class="form-group">
                             <label for="user_id">Nombre</label>
-                            <input type="text" name="name" class="form-control" value="{{ $review->name }}">
+                            <input type="text" name="name" class="form-control" value="{{ $review->name }}" readonly disabled>
                         </div>
                         <div class="form-group">
                             <label for="rating">Calificación</label>
                             <input type="number" name="rating" id="rating" class="form-control" min="1" max="5"
-                                value="{{ $review->rating }}" required>
+                                value="{{ $review->rating }}" required readonly disabled>
                         </div>
                         <div class="form-group">
                             <label for="comment">Comentario</label>
-                            <textarea name="comment" id="comment" class="form-control" rows="4" required>{{ $review->comment }}</textarea>
+                            <textarea name="comment" id="comment" class="form-control" rows="4" required readonly disabled>{{ $review->comment }}</textarea>
+                        </div>
+                        {{-- Crear radiobutton que determines si es visible o no con el valor de la base de datos llamado visible --}}
+                        <div class="form-group">
+                            <label for="visible">Visible</label><br>
+                            <input type="radio" name="visible" id="visible1" value="1" {{ $review->visible ? 'checked' : '' }} >
+                            <label for="visible1">Sí</label><br>
+                            <input type="radio" name="visible" id="visible0" value="0" {{ !$review->visible ? 'checked' : '' }} >
+                            <label for="visible0">No</label>
                         </div>
                         <button type="submit" class="btn btn-primary">Actualizar Review</button>
                     </form>
