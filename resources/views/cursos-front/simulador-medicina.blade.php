d308be860d9c36e53c056a4f9a66738c6e23161d
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-09-25 20:09:34 -0600

    Actualizacion de SEO

--- resources/views/cursos-front/simulador-medicina.blade.php
+++ resources/views/cursos-front/simulador-medicina.blade.php
@@ -2,12 +2,12 @@
 @push('meta')
     <title>{{ $meta_title ?? 'Simuladores EGEL Plus Medicina - Sapius®' }}</title>
     <meta name="description"
-        content="{{ $meta_description ?? 'Prepárate para el EGEL Plus – Medicina con los Simuladores Sapius la guía más actualizada en un solo clic.' }}">
+        content="{{ $meta_description ?? 'Prepárate para el EGEL Plus – Medicina con los Simuladores Sapius los más actualizados en un solo clic.' }}">
     <meta name="keywords"
         content="{{ $meta_keywords ?? 'Cursos Egel Plus, Cursos Enarm, Cursos Exani I, Cursos Exani II, Cursos Exani III, cursos para aprobar el EGEL, cómo aprobar el Exani' }}">
     <meta name="robots" content="{{ $meta_robots ?? 'index, follow' }}">
     <link rel="canonical" href="{{ $meta_canonical ?? url()->current() }}">
-    <meta name="image" content="{{ $meta_image ?? asset('vendor/adminmart/assets/images/big/cursos.png') }}">
+    <meta name="image" content="{{ $meta_image ?? asset('img/simuladores-medicina-SEO.jpg') }}">
 @endpush
 @push('css')
     <link rel="stylesheet" href="{{ asset('css/exani3.css') }}">
