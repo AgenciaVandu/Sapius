3a086e66257ba0bd77d3e25831e4e64611d81bff
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-12-27 18:40:49 -0600

    Endpoint

--- app/Http/Controllers/Api/N8nTestController.php
+++ app/Http/Controllers/Api/N8nTestController.php
@@ -13,9 +13,57 @@ class N8nTestController extends Controller
     {
         //enviar json a una consulta https request desde un cron de n8n omite el webhook
         $data = [
-            'status' => 'success',
-            'message' => 'Conexión exitosa',
-            'timestamp' => now()->toDateTimeString(),
+            [
+            'email' => 'test1@test.com',
+            'name' => 'Name 1',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test2@test.com',
+            'name' => 'Name 2',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test3@test.com',
+            'name' => 'Name 3',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test4@test.com',
+            'name' => 'Name 4',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test5@test.com',
+            'name' => 'Name 5',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test6@test.com',
+            'name' => 'Name 6',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test7@test.com',
+            'name' => 'Name 7',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test8@test.com',
+            'name' => 'Name 8',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test9@test.com',
+            'name' => 'Name 9',
+            'timestamp' => now()->toDateTimeString()
+            ],
+            [
+            'email' => 'test10@test.com',
+            'name' => 'Name 10',
+            'timestamp' => now()->toDateTimeString()
+            ],
+
         ];
 
         return response()->json($data);
