d6bc0dd294590ccfe76c493855d6bfb6ec5e46e2
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2026-03-02 18:14:19 -0600

    Cambios de envio de correos

--- resources/views/admin/registro/index.blade.php
+++ resources/views/admin/registro/index.blade.php
@@ -86,7 +86,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-xl">
+    <div class="modal-dialog modal-xl" style="max-width: 90%;">
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
